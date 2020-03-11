8ad6dc4c984700a74e58a6fb3db457ff3a90e0be
Kiwakosan <e0053247@u.nus.edu>
2020-03-12 03:28:29 +0800

    Signup page (without validation + connecting to postgresql)

--- customer/src/components/Register.js
+++ customer/src/components/Register.js
@@ -1,48 +1,94 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Row,Col,Container,Form} from 'react-bootstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 
 class Register extends React.Component {
+  render() {
+    return (
+      <Container fluid className="bg-white">
+        <Row>
+          <Col md={4} lg={6} className="d-none d-md-flex bg-signup"></Col>
+          <Col md={8} lg={6}>
+            <div className="login d-flex align-items-center py-5">
+              <Container>
+                <Row>
+                  <Col md={9} lg={8} className="mx-auto pl-5 pr-5">
+                    <h3 className="login-heading mb-4">
+                      Welcome to DabaoFood!
+                    </h3>
+                    <Form>
+                      <div className="form-label-group">
+                        <Form.Control
+                          type="email"
+                          id="inputEmail"
+                          placeholder="Email address"
+                        />
+                        <Form.Label htmlFor="inputEmail">
+                          Email address
+                        </Form.Label>
+                      </div>
+                      <div className="form-label-group">
+                        <Form.Control
+                          type="password"
+                          id="inputPassword"
+                          placeholder="Password"
+                        />
+                        <Form.Label htmlFor="inputPassword">
+                          Password
+                        </Form.Label>
+                      </div>
+                      <div className="form-label-group">
+                        <Form.Control
+                          type="text"
+                          id="inputAddress"
+                          placeholder="Address"
+                        />
+                        <Form.Label htmlFor="inputAddress">Address</Form.Label>
+                      </div>
+                      <div className="form-label-group">
+                        <Form.Control
+                          type="text"
+                          id="inputPostalCode"
+                          placeholder="PostalCode"
+                        />
+                        <Form.Label htmlFor="inputPostalCode">
+                          Postal Code
+                        </Form.Label>
+                      </div>
+                      <div className="form-label-group">
+                        <Form.Control
+                          type="text"
+                          id="inputPhone"
+                          placeholder="Phone"
+                        />
+                        <Form.Label htmlFor="inputPhone">
+                          Mobile Number
+                        </Form.Label>
+                      </div>
 
-	render() {
-    	return (
-    	  <Container fluid className='bg-white'>
-	         <Row>
-	            <Col md={4} lg={6} className="d-none d-md-flex bg-image"></Col>
-	            <Col md={8} lg={6}>
-	               <div className="login d-flex align-items-center py-5">
-	                  <Container>
-	                     <Row>
-	                        <Col md={9} lg={8} className="mx-auto pl-5 pr-5">
-	                           <h3 className="login-heading mb-4">New Buddy!</h3>
-	                           <Form>
-	                              <div className="form-label-group">
-	                                 <Form.Control type="email" id="inputEmail" placeholder="Email address" />
-	                                 <Form.Label htmlFor="inputEmail">Email address / Mobile</Form.Label>
-	                              </div>
-	                              <div className="form-label-group">
-	                                 <Form.Control type="password" id="inputPassword" placeholder="Password" />
-	                                 <Form.Label htmlFor="inputPassword">Password</Form.Label>
-	                              </div>
-	                              <div className="form-label-group mb-4">
-	                                 <Form.Control type="text" id="inputPromo" placeholder="Promocode" />
-	                                 <Form.Label htmlFor="inputPromo">Promocode</Form.Label>
-	                              </div>
-	                              <Link to="/login" className="btn btn-lg btn-outline-primary btn-block btn-login text-uppercase font-weight-bold mb-2">Sign Up</Link>
-	                              <div className="text-center pt-3">
-	                                 Already have an account? <Link className="font-weight-bold" to="/login">Sign In</Link>
-	                              </div>
-	                           </Form>
-	                        </Col>
-	                     </Row>
-	                  </Container>
-	               </div>
-	            </Col>
-	         </Row>
-	      </Container>
-    	);
-    }
+                      <Button
+                        type="submit"
+                        to="/login"
+                        className="btn btn-lg btn-block btn-login text-uppercase font-weight-bold mb-2"
+                      >
+                        Sign Up
+                      </Button>
+                      <div className="text-center pt-3">
+                        Already have an account?{" "}
+                        <Link className="font-weight-bold" to="/login">
+                          Sign In
+                        </Link>
+                      </div>
+                    </Form>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
-
 
 export default Register;
