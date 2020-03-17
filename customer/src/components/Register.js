705d11f15bdeeba25b8610411e5d6fb64921d14e
Kiwakosan <e0053247@u.nus.edu>
2020-03-17 20:45:41 +0800

    Added Swal

--- customer/src/components/Register.js
+++ customer/src/components/Register.js
@@ -1,27 +1,31 @@
-import React from "react";
-import { Redirect, Link } from "react-router-dom";
-import { Row, Col, Container, Form, Button, FormGroup } from "react-bootstrap";
-import FormErrors from "./FormErrors.js";
-import Swal from "sweetalert2";
-import SERVER_PREFIX from "./ServerConfig";
+import React from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import { Row, Col, Container, Form, Button, FormGroup } from 'react-bootstrap';
+import FormErrors from './FormErrors.js';
+import Swal from 'sweetalert2';
+import SERVER_PREFIX from './ServerConfig';
 
 class Register extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: "",
-      email: "",
-      password: "",
-      address: "",
-      postalCode: "",
-      phone: "",
-      formErrors: { email: "", password: "" },
+      name: '',
+      email: '',
+      password: '',
+      address: '',
+      postalCode: '',
+      phone: '',
+      formErrors: { email: '', password: '' },
       emailValid: false,
       passwordValid: false,
+      phoneValid: false,
+      postalCodeValid: false,
       formValid: false,
       redirect: false,
-      isSubmitting: false
+      isSubmitting: false,
+      requestFailed: false
     };
+    this.handleSuccess = this.handleSubmit.bind(this);
   }
 
   handleChange = event => {
@@ -41,15 +45,25 @@ class Register extends React.Component {
     let fieldValidationErrors = this.state.formErrors;
     let emailValid = this.state.emailValid;
     let passwordValid = this.state.passwordValid;
+    let phoneValid = this.state.phoneValid;
+    let postalCodeValid = this.state.postalCodeValid;
 
     switch (fieldName) {
-      case "email":
+      case 'email':
         emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        fieldValidationErrors.email = emailValid ? "" : " is in an invalid format.";
+        fieldValidationErrors.email = emailValid ? '' : ' is in an invalid format.';
         break;
-      case "password":
+      case 'password':
         passwordValid = value.length >= 7;
-        fieldValidationErrors.password = passwordValid ? "" : " must be at least 7 characters long.";
+        fieldValidationErrors.password = passwordValid ? '' : ' must be at least 7 characters long.';
+        break;
+      case 'phone':
+        phoneValid = value.match(/[0-9]{8}/g);
+        fieldValidationErrors.phone = phoneValid ? '' : ' must contains 8 digits.';
+        break;
+      case 'postalCode':
+        postalCodeValid = value.match(/[0-9]{6}/g);
+        fieldValidationErrors.postalCode = postalCodeValid ? '' : ' must contains 6 digits.';
         break;
       default:
         break;
@@ -58,7 +72,9 @@ class Register extends React.Component {
       {
         formErrors: fieldValidationErrors,
         emailValid: emailValid,
-        passwordValid: passwordValid
+        passwordValid: passwordValid,
+        phoneValid: phoneValid,
+        postalCodeValid: postalCodeValid
       },
       this.validateForm
     );
@@ -66,7 +82,8 @@ class Register extends React.Component {
 
   validateForm() {
     this.setState({
-      formValid: this.state.emailValid && this.state.passwordValid
+      formValid:
+        this.state.emailValid && this.state.passwordValid && this.state.phoneValid && this.state.postalCodeValid
     });
   }
 
@@ -85,98 +102,108 @@ class Register extends React.Component {
 
     this.setState({ isSubmitting: true });
 
-    fetch(SERVER_PREFIX + "/customers", {
-      method: "POST",
+    fetch(SERVER_PREFIX + '/customers', {
+      method: 'POST',
       body: JSON.stringify(values),
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
     })
-      .then(res => {
-        if (res === 200) {
-          Swal("Registration Successful!", "Your account has been created!", "success");
-          this.setState({ isSubmitting: false, redirect: true });
-        }
+      .then(response => {
+        if (response.status === 201) {
+          // console.log('SUCCESSS');
+          Swal.fire({
+            icon: 'success',
+            title: 'Registration Successful!',
+            timerProgressBar: true,
+            showConfirmButton: false,
+            timer: 2000
+          });
 
-        return res.json();
+          this.setState({ isSubmitting: false, redirect: true });
+          // console.log('redirect: ' + this.state.redirect);
+          return response.json();
+        } else {
+          console.log('SOMETHING WENT WRONG');
+          this.setState({ requestFailed: true });
+        }
       })
       .then(data => {
-        console.log(data);
-        !data.hasOwnProperty("error")
-          ? this.setState({ message: data.success })
-          : this.setState({ message: data.error, isError: true });
-      });
+        console.log('data: ', data);
+      })
+      .catch(console.log);
   };
 
   render() {
     if (this.state.redirect) {
-      return <Redirect to="/login" />;
+      return <Redirect to='/login' />;
     }
+
     return (
-      <Container fluid className="bg-white">
+      <Container fluid className='bg-white'>
         <Row>
-          <Col md={4} lg={6} className="d-none d-md-flex bg-signup"></Col>
+          <Col md={4} lg={6} className='d-none d-md-flex bg-signup'></Col>
           <Col md={8} lg={6}>
-            <div className="login d-flex align-items-center py-5">
+            <div className='login d-flex align-items-center py-5'>
               <Container>
                 <Row>
-                  <Col md={9} lg={8} className="mx-auto pl-5 pr-5">
-                    <h3 className="login-heading mb-4">Welcome to KinKao!</h3>
+                  <Col md={9} lg={8} className='mx-auto pl-5 pr-5'>
+                    <h3 className='login-heading mb-4'>Welcome to KinKao!</h3>
                     <p>
                       Please fill in your particulars below to get started! All fields are required.
                       {/* // Display error message here */}
                       <FormErrors formErrors={this.state.formErrors} />
                     </p>
                     <Form onSubmit={this.handleSubmit}>
-                      <FormGroup controlId="name">
+                      <FormGroup controlId='name'>
                         <Form.Control
-                          type="text"
-                          placeholder="Full Name"
+                          type='text'
+                          placeholder='Full Name'
                           value={this.state.name}
                           onChange={event => this.handleUserInput(event)}
                           required
                         />
                       </FormGroup>
-                      <FormGroup controlId="email">
+                      <FormGroup controlId='email'>
                         <Form.Control
-                          type="email"
-                          placeholder="Email address"
+                          type='email'
+                          placeholder='Email address'
                           value={this.state.email}
                           onChange={event => this.handleUserInput(event)}
                           required
                         />
                       </FormGroup>
-                      <FormGroup controlId="password">
+                      <FormGroup controlId='password'>
                         <Form.Control
-                          type="password"
-                          placeholder="Password"
+                          type='password'
+                          placeholder='Password'
                           value={this.state.password}
                           onChange={event => this.handleUserInput(event)}
                           required
                         />
                       </FormGroup>
-                      <FormGroup controlId="address">
+                      <FormGroup controlId='address'>
                         <Form.Control
-                          type="text"
-                          placeholder="Address"
+                          type='text'
+                          placeholder='Address'
                           value={this.state.address}
                           onChange={event => this.handleUserInput(event)}
                           required
                         />
                       </FormGroup>
-                      <FormGroup controlId="postalCode">
+                      <FormGroup controlId='postalCode'>
                         <Form.Control
-                          type="text"
-                          placeholder="Postal Code"
+                          type='text'
+                          placeholder='Postal Code'
                           value={this.state.postalCode}
                           onChange={event => this.handleUserInput(event)}
                           required
                         />
                       </FormGroup>
-                      <FormGroup controlId="phone">
+                      <FormGroup controlId='phone'>
                         <Form.Control
-                          type="text"
-                          placeholder="Phone"
+                          type='text'
+                          placeholder='Phone'
                           value={this.state.phone}
                           onChange={event => this.handleUserInput(event)}
                           required
@@ -185,15 +212,15 @@ class Register extends React.Component {
 
                       <Button
                         disabled={!this.state.formValid}
-                        type="submit"
-                        to="/login"
-                        className="btn btn-lg btn-block btn-login text-uppercase font-weight-bold mb-2"
+                        type='submit'
+                        to='/login'
+                        className='btn btn-lg btn-block btn-login text-uppercase font-weight-bold mb-2'
                       >
                         Sign Up
                       </Button>
-                      <div className="text-center pt-3">
-                        Already have an account?{" "}
-                        <Link className="font-weight-bold" to="/login">
+                      <div className='text-center pt-3'>
+                        Already have an account?{' '}
+                        <Link className='font-weight-bold' to='/login'>
                           Sign In
                         </Link>
                       </div>
